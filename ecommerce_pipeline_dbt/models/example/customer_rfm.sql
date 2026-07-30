57a0739f62a652ecc1407a75ba9976004d59edcd
Emmanuel <ecuwazie@gmail.com>
2026-07-30 18:26:47 -0400

    Errors fixed on stg_transactions and customer RFM build

---
+++ ecommerce_pipeline_dbt/models/example/customer_rfm.sql
@@ -0,0 +1,13 @@
+WITH max_invoice_date_cte AS (
+    SELECT
+        MAX(invoice_date) + INTERVAL '1 day' AS snapshot_date
+    FROM {{ ref('stg_transactions') }}
+)
+
+SELECT 
+t.customer_id,
+EXTRACT(DAY FROM (SELECT snapshot_date FROM max_invoice_date_cte) - t.invoice_date) AS recency,
+COUNT(DISTINCT customer_id) AS frequency,
+SUM(t.total_amount) AS monetary
+FROM {{ ref('stg_transactions') }} t
+GROUP BY t.customer_id, recency
--- ecommerce_pipeline_dbt/models/example/stg_transactions.sql
+++ ecommerce_pipeline_dbt/models/example/stg_transactions.sql
@@ -1,8 +1,8 @@
 SELECT
-CAST ('CustomerID' AS INT) as customer_id,
-CAST ('InvoiceDate' AS TIMESTAMP) as invoice_date,
-CAST ('Quantity' AS INT) as quantity,
-CAST ('UnitPrice' AS FLOAT) as unit_price,
-('Quantity' * 'UnitPrice') as total_amount
+CAST ("CustomerID" AS INT) as customer_id,
+CAST ("InvoiceDate" AS TIMESTAMP) as invoice_date,
+CAST ("Quantity" AS INT) as quantity,
+CAST ("UnitPrice" AS FLOAT) as unit_price,
+("Quantity" * "UnitPrice") as total_amount
 FROM transactions
 
